4944e7eab7fe3a4954c1e81f80fa669351b3896a
Alejandro Infante <alejandroinfante.ga@gmail.com>
2025-06-23 20:45:35 -0600

    Se agrega entity a CategoriaRequisito

--- src/main/java/mx/edut/uteq/evaluacion1/model/entity/CategoriaRequisito.java
+++ src/main/java/mx/edut/uteq/evaluacion1/model/entity/CategoriaRequisito.java
@@ -1,10 +1,13 @@
 package mx.edut.uteq.evaluacion1.model.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import lombok.Data;
+@Entity
+@Data
 public class CategoriaRequisito {
 
     @Id
