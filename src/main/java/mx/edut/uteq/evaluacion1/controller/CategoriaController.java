48a9a2e4ffd5729080d2966844aa2ab48796c14e
Alejandro Infante <alejandroinfante.ga@gmail.com>
2025-06-23 22:42:04 -0600

    Se agrega cors a catefori

--- src/main/java/mx/edut/uteq/evaluacion1/controller/CategoriaController.java
+++ src/main/java/mx/edut/uteq/evaluacion1/controller/CategoriaController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import mx.edut.uteq.evaluacion1.model.entity.Categoria;
 import mx.edut.uteq.evaluacion1.model.repository.CategoriaRepo;
 
+@CrossOrigin("http://localhost:5173/")
 @RestController
 @RequestMapping("/api/categoria")
 public class CategoriaController {
