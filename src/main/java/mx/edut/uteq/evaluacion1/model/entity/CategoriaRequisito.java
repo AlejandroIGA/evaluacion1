d3040fcf6e92f774a7ec04a62dfb94d3aaf9041a
gonzalez-aida <aidalu13oct@gmail.com>
2025-06-23 23:37:54 -0600

    correcciones en endpoint categoria-tipo

--- src/main/java/mx/edut/uteq/evaluacion1/model/entity/CategoriaRequisito.java
+++ src/main/java/mx/edut/uteq/evaluacion1/model/entity/CategoriaRequisito.java
@@ -1,10 +1,11 @@
 package mx.edut.uteq.evaluacion1.model.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 @Entity
 @Data
@@ -14,9 +15,11 @@ public class CategoriaRequisito {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "id_categoria", unique = true)
-    private int categoriaId;
-    
-    @Column(name = "id_requisito", nullable = false)
-    private int requisitoId;
+    @ManyToOne
+    @JoinColumn(name = "id_categoria", nullable = false)
+    private Categoria categoria;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tipo", nullable = false)
+    private Tipo tipo;
 }
