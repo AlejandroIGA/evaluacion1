7b56d4fe4e83ebb0f68297c6a59460e240b3e04a
gonzalez-aida <aidalu13oct@gmail.com>
2025-06-23 20:36:22 -0600

    correcciones

--- src/main/java/mx/edut/uteq/evaluacion1/model/repository/CategoriaRepo.java
+++ src/main/java/mx/edut/uteq/evaluacion1/model/repository/CategoriaRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import mx.edut.uteq.evaluacion1.model.entity.Categoria;
 
-public interface CategoriaRepo extends JpaRepository<Categoria,Long>{
+public interface CategoriaRepo extends JpaRepository<Categoria,Integer>{
     
 }
